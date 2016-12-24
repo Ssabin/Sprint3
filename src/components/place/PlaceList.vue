db2cc20dca07b8eddf386b86df1a7ad05ea49877
Max Voronov <max@maxvoronov.com>
2016-12-24 13:18:35 +0200

    added filter support- working properly only when reloading page

--- src/components/place/PlaceList.vue
+++ src/components/place/PlaceList.vue
@@ -1,6 +1,11 @@
 <template>
     <section class="place-list-section">
+         {{filteredPlaces}}
         <div class="map" ref="map"></div>
+        <button @click="removeMarkers">Remove markers</button>
+        
+       
+        
     </section>
 </template>
 
@@ -10,6 +15,7 @@
     GoogleMapsLoader.KEY = 'AIzaSyCt_f62xnUudkGEFHC7UgShw58cYlVXf24';
 
     import PlacePreview from './PlacePreview.vue'
+
     export default {
         props:{
             filteredPlaces:{
@@ -18,40 +24,87 @@
             }
         },
         data () {
-            return {        
+            return {
+                map: null,
+                markers: []
+                 
             }
         },
         methods: {
-        },
-        components:{
-          PlacePreview
-        },
-        created() {
-            GoogleMapsLoader.load(google => {
-                var myLatlng = new google.maps.LatLng(32.106529235839844, 34.83524703979492);
-                var mapOptions = {
-                    center: myLatlng,
-                    zoom: 15,
-                    zoomControl: true
-                };
+            removeMarkers() {
 
-                var map = new google.maps.Map(this.$refs.map, mapOptions);
+                for (var i = 0; i < this.markers.length; i++) {
+                    this.markers[i].setMap(null);
+                }
+                this.markers = [];
+            },
+            loadMarkers() {
+                console.log(this.filteredPlaces)
                 this.filteredPlaces.forEach(place => {
                     let lat = place.lat;
                     let lng = place.lng;
                     let myLatlng = new google.maps.LatLng(lat,lng);
                     var marker = new google.maps.Marker({
                         position: myLatlng,
-                        map: map,
-                        title: 'Hello World!',
-                        animation: google.maps.Animation.DROP
+                        map: this.map,
                      });
+                    this.markers.push(marker);
+                     
                     marker.addListener('click' , () => {
                         this.$router.push(`/place/${place.id}`);
                     });
                 });
-            });       
-        }
+            },
+            createMap() {
+                this.map = new google.maps.Map(this.$refs.map, this.mapOptions);
+                this.loadMarkers();
+            }
+        },
+        watch: {
+            filteredPlaces(curr, prev) {
+                console.log('curr', curr)
+                console.log('prev', prev)
+                this.removeMarkers();
+                if(prev.length !== 0) {
+                    this.loadMarkers();
+                }
+            }    
+        },
+        components:{
+          PlacePreview
+        },
+        // created monuted !?!
+        mounted() {
+            
+
+            GoogleMapsLoader.load(google => {
+                var myLatlng = new google.maps.LatLng(32.106529235839844, 34.83524703979492);
+                this.mapOptions = {
+                    center: myLatlng,
+                    zoom: 2,
+                    zoomControl: true
+                };
+                this.map = new google.maps.Map(this.$refs.map, this.mapOptions);
+                this.loadMarkers();
+                     
+            });
+
+            // console.log(this.filteredPlaces)
+               
+        },
+        beforeMount() {
+            }
+        // mounted() {
+        //     console.log('mounted')
+        //     console.log(this.isMapCreated)
+        //     console.log(this.$refs)
+        //     if (!this.isMapCreated) {
+        //         this.createMap()
+        //         // this.loadMarkers()
+        //         };
+            
+        // }
+           
     }
 </script>
 
